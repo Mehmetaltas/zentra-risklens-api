d5a605e21d34ec1ef48b6ffed01e73b6f3e58313
mehmet altaş <93026440+Mehmetaltas@users.noreply.github.com>
2026-03-16 19:46:21 +0300

    Update marketEngine.js

--- engines/marketEngine.js
+++ engines/marketEngine.js
@@ -1,1 +1,13 @@
+export function marketSummary(markets){
 
+return {
+
+oil:markets.oil,
+gold:markets.gold,
+sp500:markets.sp500,
+interestRate:markets.interestRate,
+shipping:markets.shipping
+
+}
+
+}
